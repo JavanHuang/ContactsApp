0f986d5d2932b044e9728aed397679e4a0ee0cb1
Javan Huang <javanhuangzx@gmail.com>
2020-12-03 00:50:26 +0800

    Adjusted CSS, added KeyboardAvoidingView to prevent keyboard covering input fields

--- screens/IndividualContactScreen.js
+++ screens/IndividualContactScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react'
-import { View, ScrollView, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { View, ScrollView, Text, TextInput, StyleSheet, Button, Platform, KeyboardAvoidingView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import colors from '../assets/colors/colors';
@@ -51,6 +51,18 @@ function IndividualContactScreen({route}) {
     )
   };
 
+  const renderInputBorder = () => {
+    return (
+      <View
+        style={{
+          borderBottomColor: colors.BORDER_COLOR,
+          borderBottomWidth: 1,
+          marginLeft: 20
+        }}
+      />
+    );
+  };
+
   useEffect(() => {
     navigation.setOptions({
       title: '',
@@ -60,125 +72,104 @@ function IndividualContactScreen({route}) {
 
   // TO-DO: To abstract the text input containers
   return (
-    <ScrollView contentContainerStyle={styles.contentContainer}>
-      <View style={styles.container}>
-        <View
-          style={{
-            paddingTop: 20,
-            paddingBottom: 40
-          }}>
-            <ContactIcon size={120}/>
+    <KeyboardAvoidingView
+      style={{
+        flex: 1,
+        color: colors.BACKGROUND_COLOR,
+        flexDirection: 'column',
+        justifyContent: 'center'
+      }}
+      behavior="padding"
+      enabled
+      keyboardVerticalOffset={10}
+    >
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <View style={styles.container}>
+          <View
+            style={{
+              paddingTop: 20,
+              paddingBottom: 40
+            }}>
+              <ContactIcon size={120}/>
+            </View>
+          <View
+            style={{
+              alignSelf: 'flex-start',
+              width: '100%',
+            }}>
+              <TextBorder text={'Main Information'} />
+              <View style={styles.textContainer}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    paddingRight: 10
+                  }}>First Name</Text>
+                <TextInput
+                  style={styles.textInputContainer}
+                  onChangeText={value => setFirstName(value)}
+                  defaultValue={contact.firstName}
+                  keyboardType={'default'}
+                  returnKeyType="next"
+                  onSubmitEditing={() => lastNameInputRef.current.focus()}
+                  blurOnSubmit={false}
+                />
+              </View>
+              {renderInputBorder()}
+              <View style={styles.textContainer}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    paddingRight: 10
+                  }}>Last Name</Text>
+                <TextInput
+                  style={styles.textInputContainer}
+                  ref={lastNameInputRef}
+                  onChangeText={value => setLastName(value)}
+                  defaultValue={contact.lastName}
+                  keyboardType={'default'} 
+                  returnKeyType="next"
+                  onSubmitEditing={() => emailInputRef.current.focus()}
+                  blurOnSubmit={false}
+                />
+              </View>
+              <TextBorder text={'Sub Information'} />
+              <View style={styles.textContainer}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    paddingRight: 10
+                  }}>Email</Text>
+                <TextInput
+                  style={styles.textInputContainer}
+                  ref={emailInputRef}
+                  onChangeText={value => setEmail(value)}
+                  value={email}
+                  keyboardType={'email-address'}
+                  returnKeyType="next"
+                  onSubmitEditing={() => phoneNumberInputRef.current.focus()}
+                  blurOnSubmit={false}
+                />
+              </View>
+              {renderInputBorder()}
+              <View style={styles.textContainer}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    paddingRight: 10
+                  }}>Phone</Text>
+                <TextInput
+                  style={styles.textInputContainer}
+                  ref={phoneNumberInputRef}
+                  onChangeText={value => setPhoneNumber(value)}
+                  value={phoneNumber}
+                  keyboardType={'default'}
+                />
+              </View>
+              {renderInputBorder()}
           </View>
-        <View
-          style={{
-            alignSelf: 'flex-start',
-            width: '100%',
-          }}>
-            <TextBorder text={'Main Information'} />
-            <View 
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                borderBottomColor: colors.BORDER_COLOR,
-                borderBottomWidth: 1,
-                paddingHorizontal: 20,
-                height: 60
-            }}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  paddingRight: 10
-                }}>First Name</Text>
-              <TextInput
-                style={{ height: 40, borderColor: colors.BORDER_COLOR, borderWidth: 1, borderRadius: 4, width: 280, paddingLeft: 10, fontSize: 16}}
-                onChangeText={value => setFirstName(value)}
-                defaultValue={contact.firstName}
-                keyboardType={'default'}
-                returnKeyType="next"
-                onSubmitEditing={() => lastNameInputRef.current.focus()}
-                blurOnSubmit={false}
-              />
-            </View>
-            <View 
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                borderBottomColor: colors.BORDER_COLOR,
-                borderBottomWidth: 1,
-                paddingHorizontal: 20,
-                height: 60
-              }}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  paddingRight: 10
-                }}>Last Name</Text>
-              <TextInput
-                style={{ height: 40, borderColor: colors.BORDER_COLOR, borderWidth: 1, borderRadius: 4, width: 280, paddingLeft: 10, fontSize: 16}}
-                ref={lastNameInputRef}
-                onChangeText={value => setLastName(value)}
-                defaultValue={contact.lastName}
-                keyboardType={'default'} 
-                returnKeyType="next"
-                onSubmitEditing={() => emailInputRef.current.focus()}
-                blurOnSubmit={false}
-              />
-            </View>
-            <TextBorder text={'Sub Information'} />
-            <View 
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                borderBottomColor: colors.BORDER_COLOR,
-                borderBottomWidth: 1,
-                paddingHorizontal: 20,
-                height: 60
-            }}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  paddingRight: 10
-                }}>Email</Text>
-              <TextInput
-                style={{ height: 40, borderColor: colors.BORDER_COLOR, borderWidth: 1, borderRadius: 4, width: 280, paddingLeft: 10, fontSize: 16}}
-                ref={emailInputRef}
-                onChangeText={value => setEmail(value)}
-                value={email}
-                keyboardType={'email-address'}
-                returnKeyType="next"
-                onSubmitEditing={() => phoneNumberInputRef.current.focus()}
-                blurOnSubmit={false}
-              />
-            </View>
-            <View 
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                borderBottomColor: colors.BORDER_COLOR,
-                borderBottomWidth: 1,
-                paddingHorizontal: 20,
-                height: 60
-            }}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  paddingRight: 10
-                }}>Phone</Text>
-              <TextInput
-                style={{ height: 40, borderColor: colors.BORDER_COLOR, borderWidth: 1, borderRadius: 4, width: 280, paddingLeft: 10, fontSize: 16}}
-                ref={phoneNumberInputRef}
-                onChangeText={value => setPhoneNumber(value)}
-                value={phoneNumber}
-                keyboardType={'default'}
-              />
-            </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -190,6 +181,26 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flexGrow: 1,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    // borderBottomColor: colors.BORDER_COLOR,
+    // borderBottomWidth: 1,
+    paddingHorizontal: 20,
+    height: 50
+  },
+  textInputContainer: {
+    height: 40,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    borderColor: colors.BORDER_COLOR,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 280,
+    paddingLeft: 10,
+    fontSize: 16
   }
 });
 
